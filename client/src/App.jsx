9b785d166be008cef98a800adfba11fde09f883c
gituser-30 <aryanmandhare30@gmail.com>
2026-01-04 12:17:40 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,30 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Login from "./pages/Login";
+// import Register from "./pages/Register";
+// import HomePage from "./pages/HomePage";
+
+// const App = () => {
+//   const token = localStorage.getItem("token");
+
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         {!token ? (
+//           <>
+//             <Route path="/" element={<Login />} />
+//             <Route path="/register" element={<Register />} />
+//           </>
+//         ) : (
+//           <Route path="/" element={<HomePage />} />
+//         )}
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// };
+
+// export default App;
+
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import HomePage from "./pages/HomePage";
@@ -9,14 +35,18 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        {!token ? (
-          <>
-            <Route path="/" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-          </>
-        ) : (
-          <Route path="/" element={<HomePage />} />
-        )}
+        {/* Public routes */}
+        <Route path="/login" element={!token ? <Login /> : <Navigate to="/" />} />
+        <Route
+          path="/register"
+          element={!token ? <Register /> : <Navigate to="/" />}
+        />
+
+        {/* Protected route */}
+        <Route
+          path="/"
+          element={token ? <HomePage /> : <Navigate to="/login" />}
+        />
       </Routes>
     </BrowserRouter>
   );
