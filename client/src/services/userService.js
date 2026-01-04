3d21611c31a82ad35ce1b85ab3f9e33b299b26f1
gituser-30 <aryanmandhare30@gmail.com>
2026-01-04 19:16:58 +0530

    Update userService.js

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -1,23 +1,38 @@
 import axios from "axios";
-const API_URL = "https://chatapplication-z7kn.onrender.com/api/users";
+const API_BASE =
+  import.meta.env.PROD
+    ? "https://dbatuscholarhub-chatapp.onrender.com"
+    : "http://localhost:5000";
 
 // ethun apan sagle user fetch karnar ahot
-export const getUsers = async()=>{
-    const token = localStorage.getItem("token");
+// export const getUsers = async()=>{
+//     const token = localStorage.getItem("token");
 
-    const response = await axios.get(API_URL,{
-        headers: {
-            Authorization: `Bearer ${token}`,
-        },
-    });
-    return response.data;
+//     const response = await axios.get(API_BASE,{
+//         headers: {
+//             Authorization: `Bearer ${token}`,
+//         },
+//     });
+//     return response.data;
+// };
+
+export const getUsers = async () => {
+  const token = localStorage.getItem("token");
+
+  const response = await axios.get(`${API_BASE}/api/users`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return response.data;
 };
 
 export const blockUser = async (userId) => {
   const token = localStorage.getItem("token");
 
-  const response = await axios.post(
-    `${API_URL}/block/${userId}`,
+  const response = await axios.put(
+    `${API_BASE}/api/users/block/${userId}`,
     {},
     {
       headers: {
