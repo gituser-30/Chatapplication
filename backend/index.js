66e81ecc7fd3fa38f6df133417ba6d93dc390bcf
gituser-30 <aryanmandhare30@gmail.com>
2026-01-04 11:18:24 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,7 @@ app.use("/api/auth",authRoutes);
 app.use(express.static(path.join(__dirname, "client", "dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "client", "index.html"));
 });
 
 
