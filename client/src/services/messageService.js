ba67c07e8ca83ac3ff0d529ed4760bfa5df770b0
gituser-30 <aryanmandhare30@gmail.com>
2026-01-04 10:21:42 +0530

    Update messageService.js

--- client/src/services/messageService.js
+++ client/src/services/messageService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/messages";
+const API_URL = "https://chatapplication-z7kn.onrender.com/api/messages";
 
 // get messages between logged-in user & selected user
 export const getMessages = async (userId) => {
