b16ea6b3530fcc6f179239d9df9960a351185765
gituser-30 <aryanmandhare30@gmail.com>
2026-01-04 12:26:58 +0530

    Update authService.js

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -5,7 +5,11 @@ const API_URL = "https://chatapplication-z7kn.onrender.com/api/auth";
 // Register API
 
 export const registerUser = async (userData)=>{
-    const response = await axios.post(`${API_URL}/register`,userData);
+    const response = await axios.post(`${API_URL}/register`,userData, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
     return response.data;
 };
 
